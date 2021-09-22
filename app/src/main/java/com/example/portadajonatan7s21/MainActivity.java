b9f03123da900a0771fe3cae7eaccabc9995fe91
JonatanAlexisYanezRivas <jonatanalexis@live.com>
2021-09-21 19:48:56 -0500

    commit

---
+++ app/src/main/java/com/example/portadajonatan7s21/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.portadajonatan7s21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
